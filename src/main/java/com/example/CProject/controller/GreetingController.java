b6d5f2c1ac9e9c44c4e0e48671264f09e1244eda
JISOO <kwonjisoo14@gmail.com>
2023-10-31 03:07:06 +0900

    join,login,choice 프론트 구현

--- src/main/java/com/example/CProject/controller/GreetingController.java
+++ src/main/java/com/example/CProject/controller/GreetingController.java
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class GreetingController {
 
-    @GetMapping("/greeting")
+    @GetMapping("/join")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
-        return "greeting";
+        return "join";
     }
 
 }
